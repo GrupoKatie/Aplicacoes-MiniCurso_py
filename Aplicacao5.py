094db2137482e142ace6ef74e92599c53adb8c27
Kamila Benevides <kamilaalmeida1997@gmail.com>
2020-05-18 14:23:00 -0300

    Create Aplicacao5.py

---
+++ Aplicacao5.py
@@ -0,0 +1,10 @@
+espaco = float(input())
+tempo = float(input())
+velocidade = float(input())
+
+velocidadeMedia = espaco / tempo
+
+aceleracao = velocidadeMedia / tempo
+
+print(velocidadeMedia)
+print(aceleracao)
