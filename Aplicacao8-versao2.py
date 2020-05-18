daed2453530c9cb7437b698f18367e7d287fc823
Kamila Benevides <kamilaalmeida1997@gmail.com>
2020-05-18 16:09:38 -0300

    Create Aplicacao8-versao2.py

---
+++ Aplicacao8-versao2.py
@@ -0,0 +1,19 @@
+meusDados = {}
+
+nome = input()
+idade = input()
+cidade = input()
+estado = input()
+universidade = input()
+curos = input()
+periodo = input()
+
+meusDados['nome'] = nome
+meusDados['idade'] = idade
+meusDados['cidade'] = cidade
+meusDados['estado'] = estado
+meusDados['universidade'] = universidade
+meusDados['curso'] = curos
+meusDados['periodo'] = periodo
+
+print(meusDados)
