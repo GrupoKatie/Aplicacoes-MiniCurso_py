74d7aa061aabc2de07bde8c47965bf03e4c1e341
Kamila Benevides <kamilaalmeida1997@gmail.com>
2020-05-18 12:23:38 -0300

    Create Aplicacao3.py

---
+++ Aplicacao3.py
@@ -0,0 +1,9 @@
+segundos = int(input())
+horas = segundos // 3600
+segundos = segundos % 3600
+minutos = segundos // 60
+segundos = segundos - minutos * 60
+
+print(horas, "horas")
+print(minutos, "minutos")
+print(segundos, "segundos")
