4f97a9e6bf00aa27cad5011074da31db570c2f5f
Kamila Benevides <kamilaalmeida1997@gmail.com>
2020-05-18 20:33:43 -0300

    Update Aplicacao5.py

--- Aplicacao5.py
+++ Aplicacao5.py
@@ -1,10 +1,12 @@
+def funcVelo(velocidade, tempo, espaco):
+  velocidadeMedia = espaco / tempo
+  aceleracao = velocidadeMedia / tempo
+  print(velocidadeMedia)
+  print(aceleracao)
+  
+  
 espaco = float(input())
 tempo = float(input())
 velocidade = float(input())
 
-velocidadeMedia = espaco / tempo
-
-aceleracao = velocidadeMedia / tempo
-
-print(velocidadeMedia)
-print(aceleracao)
+funcVelo(velocidade, tempo, espaco)
