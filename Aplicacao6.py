fbf9a29f1f3bcd45bdaa029bcbf81b0211c40f4a
Kamila Benevides <kamilaalmeida1997@gmail.com>
2020-05-18 14:29:44 -0300

    Create Aplicacao6.py

---
+++ Aplicacao6.py
@@ -0,0 +1,11 @@
+i = 1
+while i < 200:
+    if i % 2 == 0:
+        print(i)
+    i += 1
+
+contador = 0
+for i in range(200):
+    if i % 2 == 0:
+        contador += 1
+print("quantidade de numeros pares entre 0 e 200 = ", contador)
